4cf34d6048e8832d5a2fcef495641e0370c68712
rob662002 <robertdowney662002@gmail.com>
2023-05-07 15:39:45 +0530

    first commit

---
+++ src/components/Analysis.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Laptop from '../assets/laptop.jpg'
+
+const Analysis = () => {
+  return (
+    <div className='w-full bg-white'>
+        <div className='max-w-[1240px] grid md:grid-cols-2 mx-auto'>
+            <img src={Laptop} alt='/'/>
+            <div className='flex flex-col my-auto text-center'>
+                <h1 className='text-3xl font-bold'>Manage Data Analytics</h1>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatem rem ea non facere sequi, officia quia fugit at, distinctio voluptate consequuntur eos laborum maiores officiis delectus nisi. Officia, sunt?
+                </p>
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Analysis
---
+++ src/components/Login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Hall from '../assets/halls.jpg'
+import Navbar from './Navbar'
+import '../index.css'
+
+const Login = () => {
+  return (
+    <>  
+        <Navbar/>        
+        <div className='grid h-screen w-full grid-cols-1 md:grid-cols-2'>
+            <div className='max-h-full hidden md:block'>
+                <img className='w-full h-screen object-contain' src={Hall} alt='/'/>
+            </div>
+            <div className='bg-[#000300] flex flex-col justify-center'>
+            <form className='max-w-[400px] w-full mx-auto bg- p-4 border border-white rounded-sm text-white'>
+                    <h2 className='text-4xl font-bold text-center py-6'>MITS HALLS.</h2>
+                    <div className='flex flex-col py-2'>
+                        <label>Email</label>
+                        <input type='text' className='border py-2' />
+                    </div>
+                    <div className='flex flex-col py-2'>
+                        <label>Password</label>
+                        <input type='password' className='border py-2' />    
+                    </div>
+                    <button className='text-black border w-full my-5 py-2 bg-[#00df9a] '>Sign In</button>
+                    <div className='text-center'> 
+                        <p>Create an account</p>
+                    </div>            
+                </form>     
+            </div>      
+        </div>
+        </>  
+  )
+}
+
+export default Login
