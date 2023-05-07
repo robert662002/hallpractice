1e2d438dfe98d93d37da2908b81f4158bb90b7d6
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2023-05-07 16:49:14 +0530

    Changes to make login under the navbar

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,33 +4,30 @@ import Navbar from './Navbar'
 import '../index.css'
 
 const Login = () => {
-  return (
-    <>  
-        <Navbar/>        
-        <div className='grid h-screen w-full grid-cols-1 md:grid-cols-2'>
-            <div className='max-h-full hidden md:block'>
-                <img className='w-full h-screen object-contain' src={Hall} alt='/'/>
+    return (
+            <div className='absolute grid mt-[38.5rem] w-full grid-cols-1 md:grid-cols-2 -z-10'>
+                <div className='max-h-full hidden md:block relative -left-24'>
+                    <img className='w-full h-screen object-contain' src={Hall} alt='/' />
+                </div>
+                <div className='bg-[#000300] flex flex-col justify-center'>
+                    <form className='max-w-[400px] w-full mx-auto bg- p-4 border border-white rounded-sm text-white'>
+                        <h2 className='text-4xl font-bold text-center py-6'>MITS HALLS.</h2>
+                        <div className='flex flex-col py-2'>
+                            <label>Email</label>
+                            <input type='text' className='border py-2' />
+                        </div>
+                        <div className='flex flex-col py-2'>
+                            <label>Password</label>
+                            <input type='password' className='border py-2' />
+                        </div>
+                        <button className='text-black border w-full my-5 py-2 bg-[#00df9a] '>Sign In</button>
+                        <div className='text-center'>
+                            <p>Create an account</p>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className='bg-[#000300] flex flex-col justify-center'>
-            <form className='max-w-[400px] w-full mx-auto bg- p-4 border border-white rounded-sm text-white'>
-                    <h2 className='text-4xl font-bold text-center py-6'>MITS HALLS.</h2>
-                    <div className='flex flex-col py-2'>
-                        <label>Email</label>
-                        <input type='text' className='border py-2' />
-                    </div>
-                    <div className='flex flex-col py-2'>
-                        <label>Password</label>
-                        <input type='password' className='border py-2' />    
-                    </div>
-                    <button className='text-black border w-full my-5 py-2 bg-[#00df9a] '>Sign In</button>
-                    <div className='text-center'> 
-                        <p>Create an account</p>
-                    </div>            
-                </form>     
-            </div>      
-        </div>
-        </>  
-  )
+    )
 }
 
 export default Login
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
 import { useState } from 'react'
-const Navbar = () => {
+import Login from './Login'
+const Navbar = ({children}) => {
   const [nav,setNav] = useState(true)
   const handleNav =()=>{
     setNav(!nav)
   }
   return (
-    <div className='sticky top-0 flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+    <div className='relative top-0 flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>MITS HALLS.</h1>
         <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
@@ -29,6 +30,7 @@ const Navbar = () => {
               <li className='p-4'>Contact</li>
           </ul>
         </div>
+        {children}
     </div>
   )
 }
